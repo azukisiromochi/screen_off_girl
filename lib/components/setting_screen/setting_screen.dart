525d8e5523a5f50cb9b03f695c1a6102631a574f
azukisiromochi <azukisiromochi@gmail.com>
2021-04-15 17:26:08 +0900

    ✨ settings_ui 導入してみた

---
+++ lib/components/setting_screen/setting_screen.dart
@@ -0,0 +1,80 @@
+import 'package:flutter/material.dart';
+import 'package:settings_ui/settings_ui.dart';
+
+class SettingsScreen extends StatefulWidget {
+  @override
+  _SettingsScreenState createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
+  bool lockInBackground = true;
+  bool notificationsEnabled = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Settings UI')),
+      body: buildSettingsList(),
+    );
+  }
+
+  Widget buildSettingsList() {
+    return SettingsList(
+      sections: [
+        SettingsSection(
+          title: 'Account',
+          tiles: const [
+            SettingsTile(title: 'Phone number', leading: Icon(Icons.phone)),
+            SettingsTile(title: 'Email', leading: Icon(Icons.email)),
+            SettingsTile(title: 'Sign out', leading: Icon(Icons.exit_to_app)),
+          ],
+        ),
+        SettingsSection(
+          title: 'Security',
+          tiles: [
+            SettingsTile.switchTile(
+              title: 'Lock app in background',
+              leading: const Icon(Icons.phonelink_lock),
+              switchValue: lockInBackground,
+              onToggle: (bool value) {
+                setState(() {
+                  lockInBackground = value;
+                  notificationsEnabled = value;
+                });
+              },
+            ),
+            SettingsTile.switchTile(
+                title: 'Use fingerprint',
+                subtitle: 'Allow application to access stored fingerprint IDs.',
+                leading: const Icon(Icons.fingerprint),
+                onToggle: (bool value) {},
+                switchValue: false),
+            SettingsTile.switchTile(
+              title: 'Change password',
+              leading: const Icon(Icons.lock),
+              switchValue: true,
+              onToggle: (bool value) {},
+            ),
+            SettingsTile.switchTile(
+              title: 'Enable Notifications',
+              enabled: notificationsEnabled,
+              leading: const Icon(Icons.notifications_active),
+              switchValue: true,
+              onToggle: (value) {},
+            ),
+          ],
+        ),
+        SettingsSection(
+          title: 'Misc',
+          tiles: const [
+            SettingsTile(
+                title: 'Terms of Service', leading: Icon(Icons.description)),
+            SettingsTile(
+                title: 'Open source licenses',
+                leading: Icon(Icons.collections_bookmark)),
+          ],
+        ),
+      ],
+    );
+  }
+}
